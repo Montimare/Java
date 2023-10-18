cf687378e9578f6fad808dba6bc38e1e54540886
MarcRoemerGKGAB <marc.roemer@glueckkanja-gab.com>
2023-10-18 13:15:20 +0200

    first commit

---
+++ ab2/A2.java
@@ -0,0 +1,43 @@
+package ab2;
+
+public class A2 {
+    public static void main(String[] args) {
+        int value = 3;
+        switch (value) {
+            case 0:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+        // for (int i = 0; i < 1000; i += 50) {
+        //     System.out.println(i);
+        // }
+
+        // for (int i = 0; i < 999; i++) {
+        //     if (i % 50 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+        // int i = 0;
+        // while (i < 1000) {
+        //     if (i % 50 == 0){
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        // }
+        
+        // int i = 0;
+        // do {
+        //     if (i % 50 == 0){
+        //         System.out.println(i);
+        //     }
+        //     i++;
+        // } while (i < 1000);
+        
+
+    }
+}
