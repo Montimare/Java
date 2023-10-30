bd323f5cb28e4b2a8e5b4e7c29ef2e989af4df79
MarcRoemerGKGAB <marc.roemer@glueckkanja-gab.com>
2023-10-30 18:48:19 +0100

    better output

--- Ab3/Card.java
+++ Ab3/Card.java
@@ -16,6 +16,6 @@ public class Card {
     }
 
     public String toString(){
-        return (value + " of " + color + ", ");
+        return (value + " of " + color);
     }
 }
--- Ab3/CardDeck.java
+++ Ab3/CardDeck.java
@@ -7,7 +7,8 @@ import java.util.ArrayList;
 
 public class CardDeck {
     ArrayList<ArrayList<Card>> deck = new ArrayList<>();
-    CardDeck(){
+
+    CardDeck() {
         for (int enumeratorColor = 0; enumeratorColor < COLOR.values().length; enumeratorColor++) {
             ArrayList<Card> tmp = new ArrayList<>();
             for (int enumeratorValue = 0; enumeratorValue < VALUE.values().length; enumeratorValue++) {
@@ -18,15 +19,15 @@ public class CardDeck {
         // System.out.println("123");
     }
 
-    public String toString(){
+    public String toString() {
         String result = "";
-        for (ArrayList<Card> itemArrayList: deck){
+        for (ArrayList<Card> itemArrayList : deck) {
             for (Card item2 : itemArrayList) {
-                result += item2.toString();
+                result += (item2.toString() + ", ");
             }
-            result += "\n";
+            result = result.substring(0, result.length() - 2);
+            result += "\n\n";
         }
         return result;
     }
 }
-
